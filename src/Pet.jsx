23544ebb5a86291ade78a18c9d58e723706a3f6b
olganovichkova <58965002+olganovichkova@users.noreply.github.com>
2024-02-19 20:35:48 -0700

    fixed api block

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  let hero = "https://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -4,7 +4,7 @@ const fetchBreedList = async ({ queryKey }) => {
   if (!animal) return [];
 
   const apiRes = await fetch(
-    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
 
   if (!apiRes.ok) {
